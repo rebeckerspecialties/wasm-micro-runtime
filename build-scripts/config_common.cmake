rebeckerspecialties/wasm-micro-runtime

commit c357ac1e1f944208d6c7a1b0677dabfcef58f7a1

Fix -m32 unrecognized issue when compile 32-bit target on some 64-bit systems (#866)
When compile 32-bit targets on some 64-bit systems, the "-m32" flag might be
unrecognized by some gcc compilers, e.g. compiling arm32 in aarch64 system,
compiling riscv32 in riscv64 system.
Add check before adding "-m32" flag to gcc, and only add it if it is supported.

build-scripts/config_common.cmake
@@ -61,9 +61,13 @@ if (CMAKE_SIZEOF_VOID_P EQUAL 8)
       set (CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "${CMAKE_SHARED_LIBRARY_LINK_C_FLAGS} -fPIC")
     endif ()
   else ()
-    add_definitions (-m32)
-    set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -m32")
-    set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -m32")
+    include(CheckCCompilerFlag)
+    Check_C_Compiler_Flag( -m32  M32_OK )
+    if (M32_OK)
+      add_definitions (-m32)
+      set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -m32")
+      set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -m32")
+    endif ()
   endif ()
 endif ()
 

commit 68ddc9cd012e028a9e9351f9ef77edfa926bff4e

Fix compile error when build target X86_32 (#869)
Fix compile error introduced by #866.

build-scripts/config_common.cmake
@@ -62,8 +62,8 @@ if (CMAKE_SIZEOF_VOID_P EQUAL 8)
     endif ()
   else ()
     include(CheckCCompilerFlag)
-    Check_C_Compiler_Flag( -m32  M32_OK )
-    if (M32_OK)
+    Check_C_Compiler_Flag(-m32 M32_OK)
+    if (M32_OK OR WAMR_BUILD_TARGET STREQUAL "X86_32")
       add_definitions (-m32)
       set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -m32")
       set (CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -m32")