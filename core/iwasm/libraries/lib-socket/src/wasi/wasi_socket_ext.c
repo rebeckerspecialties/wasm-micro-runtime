rebeckerspecialties/wasm-micro-runtime

commit 9ea13dd2d2931e8a8047feb0d7444eff2c36054f

Small change to make init of vars buildable in clang and clang++ (#1590)

core/iwasm/libraries/lib-socket/src/wasi/wasi_socket_ext.c
@@ -90,9 +90,11 @@ wasi_addr_to_sockaddr(const __wasi_addr_t *wasi_addr,
     switch (wasi_addr->kind) {
         case IPv4:
         {
-            struct sockaddr_in sock_addr_in = { 0 };
+            struct sockaddr_in sock_addr_in;
             uint32_t s_addr;
 
+            memset(&sock_addr_in, 0, sizeof(sockaddr_in));
+            
             s_addr = (wasi_addr->addr.ip4.addr.n0 << 24)
                      | (wasi_addr->addr.ip4.addr.n1 << 16)
                      | (wasi_addr->addr.ip4.addr.n2 << 8)
@@ -108,7 +110,10 @@ wasi_addr_to_sockaddr(const __wasi_addr_t *wasi_addr,
         }
         case IPv6:
         {
-            struct sockaddr_in6 sock_addr_in6 = { 0 };
+            struct sockaddr_in6 sock_addr_in6;
+            
+            memset(&sock_addr_in6, 0, sizeof(sockaddr_in6));
+
             uint16_t *addr_buf = (uint16_t *)sock_addr_in6.sin6_addr.s6_addr;
 
             addr_buf[0] = htons(wasi_addr->addr.ip6.addr.n0);

commit 0f6615036cf21ef60c96c532293b1f1c293c629b

Fix build socket api error (#1610)
Fix build socket api error introduced by #1590

core/iwasm/libraries/lib-socket/src/wasi/wasi_socket_ext.c
@@ -93,7 +93,7 @@ wasi_addr_to_sockaddr(const __wasi_addr_t *wasi_addr,
             struct sockaddr_in sock_addr_in;
             uint32_t s_addr;
 
-            memset(&sock_addr_in, 0, sizeof(sockaddr_in));
+            memset(&sock_addr_in, 0, sizeof(sock_addr_in));
 
             s_addr = (wasi_addr->addr.ip4.addr.n0 << 24)
                      | (wasi_addr->addr.ip4.addr.n1 << 16)
@@ -112,7 +112,7 @@ wasi_addr_to_sockaddr(const __wasi_addr_t *wasi_addr,
         {
             struct sockaddr_in6 sock_addr_in6;
 
-            memset(&sock_addr_in6, 0, sizeof(sockaddr_in6));
+            memset(&sock_addr_in6, 0, sizeof(sock_addr_in6));
 
             uint16_t *addr_buf = (uint16_t *)sock_addr_in6.sin6_addr.s6_addr;
 